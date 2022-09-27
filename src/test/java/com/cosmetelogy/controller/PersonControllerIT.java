788bfdc995ded8337f95a5421642496a565252d6
Katrin <Karkusha869@gmail.com>
2022-09-27 19:23:37 +0300

    commit after tests

--- src/test/java/com/cosmetelogy/controller/PersonControllerIT.java
+++ src/test/java/com/cosmetelogy/controller/PersonControllerIT.java
@@ -1,6 +1,7 @@
 package com.cosmetelogy.controller;
 
 import com.cosmetelogy.dto.PersonDto;
+import com.cosmetelogy.dto.ProductDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -31,7 +32,7 @@ class PersonControllerIT {
                 .post(path)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(personDto));
-        String expected="1";
+        String expected = "1";
         //When
         MvcResult mvcResult = mockMvc.perform(postPerson)
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
@@ -43,11 +44,39 @@ class PersonControllerIT {
     }
 
     @Test
-    void testSave() {
+    void testSave() throws Exception {
+        //Given
+        String path = "/person/save/product";
+        ProductDto productDto = new ProductDto(2L, "Serum", 6.5, null, "naturally", "DKM");
+        MockHttpServletRequestBuilder postProduct = MockMvcRequestBuilders
+                .post(path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productDto));
+        String expected = "2";
+
+        //When
+        MvcResult mvcResult = mockMvc.perform(postProduct)
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        //Then
+        Assertions.assertEquals(expected, mvcResult.getResponse().getContentAsString());
     }
 
     @Test
-    void findById() {
+    void findById() throws Exception {
+        //Given
+        String path = "/person/id";
+        MockHttpServletRequestBuilder getId = MockMvcRequestBuilders
+                .post(path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(path));
+        String expected = "Sorry, we have some problem now. Please try later.";
+        //When
+
+        MvcResult mvcResult = mockMvc.perform(getId)
+                .andReturn();
+        //Then
+        Assertions.assertEquals(expected, mvcResult.getResponse().getContentAsString());
     }
 
     @Test
